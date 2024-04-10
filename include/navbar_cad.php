0a9430d529a6eb7c89353f566a5ce38ad1043397
emerson096 <116975799+emerson096@users.noreply.github.com>
2024-04-09 22:17:08 -0300

    Add files via upload

---
+++ include/navbar_cad.php
@@ -0,0 +1,15 @@
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="navbar_cad.css">
+
+</head>
+<body>
+    <!-- Barra de navegação -->
+    <div class="navbar">
+      <div class="logo">
+        <img id="logo" src="logo.png" alt="">
+      </div>
+      <button class="inicio" id="inicio">Início</button>
+        
+    </div>
+    <!-- Linha abaixo do navbar -->
+    <div class="l1"></div>
